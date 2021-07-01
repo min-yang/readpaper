15767427fe58ea89e65e192cbe7aa459a8657c28
杨珉 <yangm@corp.21cn.com>
2021-07-01 17:42:33 +0800

    模块化、新增论文正文页并列出相似文献

--- readpaper/server.py
+++ readpaper/server.py
@@ -4,12 +4,14 @@ import random
 import tornado.ioloop
 import tornado.web
 import tornado.options
+import pymongo
 from pymongo import MongoClient
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        num_skip = random.randint(0, self.application.mongo_client.paper.cs_paper_abs.estimated_document_count()-1)
+        page = int(self.get_query_argument('page', 1))
         papers = []
+
         for doc in self.application.mongo_client.paper.cs_paper_abs.find(skip=num_skip, limit=10):
             papers.append(doc)
             
@@ -25,22 +27,33 @@ class TopicHandler(tornado.web.RequestHandler):
         if num_skip < 0:
             raise tornado.web.HTTPError(404)
             
-        for doc in self.application.mongo_client.paper.cs_paper_abs.find(filter, skip=num_skip, limit=11):
+        for doc in self.application.mongo_client.paper.cs_paper_abs.find(filter, skip=num_skip, limit=11, sort=[('updated', pymongo.DESCENDING)]):
             papers.append(doc)
             
         if not papers:
             raise tornado.web.HTTPError(404)
             
-        self.render('paper.html', papers=papers, title='%s 主题论文列表' %topic_index, page=page)
+        self.render('paper.html', papers=papers, title='主题-%s 论文列表' %topic_index, page=page)
 
 class PaperHandler(tornado.web.RequestHandler):
-    pass
+    def get(self, paper_id):
+        paper = self.application.mongo_client.paper.cs_paper_abs.find_one({'_id': paper_id})
+        if not paper:
+            raise tornado.web.HTTPError(404)
+            
+        sim_title = []
+        sim_ids = paper['similar_paper']
+        for sim_id in sim_ids:
+            title = self.application.mongo_client.paper.cs_paper_abs.find_one({'_id': sim_id})['title']
+            sim_title.append((sim_id, title))
+            
+        self.render('paper_text.html', paper=paper, sim_title=sim_title)
 
 class Application(tornado.web.Application):
     def __init__(self):
         self.mongo_client = MongoClient()
         handlers = [
-            (r"/", MainHandler),
+            (r"/paper/all", MainHandler),
             (r"/topic/([0-9]+)", TopicHandler),
             (r"/paper/([0-9.]+)", PaperHandler),
         ]
